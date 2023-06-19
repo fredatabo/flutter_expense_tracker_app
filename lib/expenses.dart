318ef4fa65b336c464f4e82aa70f0d011fc74a7c
fredatabo <fredatabo@gmail.com>
2023-06-19 20:07:40 +0100

    A bar chart showing expenses analysis was added

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker/new_expense.dart';
 import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -81,7 +82,9 @@ class _ExpensesState extends State<Expenses> {
         ),
       ]),
       body: Column(
-        children: [const Text('The chart'), Expanded(child: mainContent)],
+        children: [const Text('The chart'), 
+        Chart(expenses: _registeredExpenses),
+        Expanded(child: mainContent)],
       ),
     );
   }
